d5cc21a958d6927088f5bf2fda31f509f79a63f8
Mikko Heikkinen <hawk@biomi.org>
2012-10-21 10:10:52 +0300

    List structure

--- wallpaper/index.php
+++ wallpaper/index.php
@@ -17,8 +17,16 @@ header('Content-Type: text/html; charset=utf-8');
 
 <p>Näin saat ilmanlaadun mukaan vaihtuvan taustavärin älypuhelimeesi:</p>
 <ol>
-<li>Asenna jokin sovellus, joka näyttää verkkosivun taustakuvana,<br /> esim. Androidilla <a href="https://play.google.com/store/apps/details?id=com.yaji.weblivewallpaper">Web LiveWallpaper</a> tai <a href="https://play.google.com/store/apps/details?id=com.dngames.websitelivewallpaper">WebLiveWallpaper</a> (<a href="https://github.com/mikkohei13/airquality-finland/blob/master/mobile.md">käyttöohjeita</a>)</li>
-<li>Valitse ao. luettelosta sopivin mittausasema (<a href="http://www.biomi.org/blogi/2012/10/ilmanlaatu-puhelin/">Miksi paikkaa ei valita automaattisesti?</a>)</li>
+<li>Asenna jokin sovellus, joka näyttää verkkosivun taustakuvana,<br /> esim. Androidilla <a href="https://play.google.com/store/apps/details?id=com.yaji.weblivewallpaper">Web LiveWallpaper</a> tai <a href="https://play.google.com/store/apps/details?id=com.dngames.websitelivewallpaper">WebLiveWallpaper</a> 
+	<ul><li>
+	(<a href="https://github.com/mikkohei13/airquality-finland/blob/master/mobile.md">Käyttöohjeita</a>)
+	</li></ul>
+</li>
+<li>Valitse ao. luettelosta sopivin mittausasema
+	<ul><li>
+	(<a href="http://www.biomi.org/blogi/2012/10/ilmanlaatu-puhelin/">Miksi paikkaa ei valita automaattisesti?</a>)
+	</li></ul>
+</li>
 <li>Aseta sivu taustakuvaksi. Em. sovelluksissa tämä tapahtuu painamalla Menu > Share > sovellus</li>
 </ol>
 
